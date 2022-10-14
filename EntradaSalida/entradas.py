4e7c24b236978988968bfaf69763a22f557cb2b1
padillasam <panarick@gmail.com>
2022-10-14 11:37:08 -0500

    entrada salida practica

---
+++ EntradaSalida/entradas.py
@@ -0,0 +1,11 @@
+a=int(input('Ingrese cateto adyacente'))
+b=int(input('Ingrese cateto adyacente'))
+
+c=((a**2)+(b**2))**0.5
+print('La hipotenusa es: ',c)
+
+
+"""nombre='Pedro Perez'
+print(nombre)
+nombre=input('Ingrese su nombre...')"""
+#print('nombre:',nombre);
---
+++ EntradaSalida/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+a=input('Ingrese cateto adyacente')
+b=input('Ingrese cateto adyacente')
+c=((a**2)+(b**2))**0.5
+print('La hipotenusa es: ',c)
