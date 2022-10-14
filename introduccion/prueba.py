1c6c56c03a6feab1512e46e618de8b92187d7bef
padillasam <panarick@gmail.com>
2022-10-14 10:53:45 -0500

    primera versión curso python

---
+++ introduccion/prueba.py
@@ -0,0 +1,12 @@
+#print('Curso exitoso de python CLBDD adso 2560664',x)
+#print('Seremes felices')
+x=100
+print('Ingrese el valor de x')
+x=int(input())
+#print('Cuadrado de x es: {0}'.format(x*x))
+print(f"Cuadrado de x es: {x*x}")
+print(f"{x} * {x} = {x*x}")
+print('doble de x=',(x+x))
+b=True
+flag:bool=False
+print('flag= ',flag)
---
+++ introduccion/prueba1.py
@@ -0,0 +1,5 @@
+user=input('Aprendizaje de python, cual es su nombre\n')
+print('bienvenido ',user)
+lang=input('Favorite programming language')
+#print(lang,'is','fun',sep='*',end='!....\n');
+print(lang,'is','fun',sep='*',end='\n');
