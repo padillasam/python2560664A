0c95dce8434e26eb9694ce80291fba618950ea85
padillasam <panarick@gmail.com>
2022-10-19 11:37:58 -0500

    modulo math

--- introduccion/MathAndRound.py
+++ introduccion/MathAndRound.py
@@ -1,6 +1,16 @@
+#funciones matemáticas
+
 import math
 import random
+base=0
+exp=0
+base=int(input('ingrese base'))
+exp=int(input('ingrese exponente'))
+print('rta= ',math.pow(base,exp))
+
+
 print(round(random.random()*10))
 print(math.pow(2,5))
 print(math.sqrt(100))
 print(round(random.random()*100))
+
--- introduccion/tempCodeRunnerFile.py
+++ introduccion/tempCodeRunnerFile.py
@@ -1,2 +1,9 @@
+#funciones matemáticas
+
+import math
 import random
-print(round(random.random()*10))
+base=0
+exp=0
+base=int(input('ingrese base'))
+exp=int(input('ingrese exponente'))
+print('rta= ',math.pow(base,exp))
