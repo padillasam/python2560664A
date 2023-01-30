c679bf96b3cb25dbf5caf01758ab8e657b9b82eb
padillasam <panarick@gmail.com>
2023-01-30 11:53:00 -0500

    actrualizacion modulos

--- usomodulos/prueba.py
+++ usomodulos/prueba.py
@@ -1,13 +1,12 @@
-import sys
 from sys import path
-#path.append('..\\MiPracticaPythonA\\modulos')
 
 path.append('C:\\Users\\usuario\\Dropbox\\sena2022\\Trimestre4-06octubre-17diciembre\\MiPracticaPythonA\\modulos')
 
-import module
+#path.append('..\\MiPracticaPythonA\\modulos')
+import modulo1
 from modulo1 import suml,prodl
 
 zeroes = [0 for i in range(5)]
 ones = [1 for i in range(5)]
-print(module.suml(zeroes))
-print(module.prodl(ones))
+print(modulo1.suml(zeroes))
+print(modulo1.prodl(ones))
