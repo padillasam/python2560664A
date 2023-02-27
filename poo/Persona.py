1486c5eb5eee1a85058cde3016da84f47a3b3c70
padillasam <panarick@gmail.com>
2023-02-27 11:33:51 -0500

    intro POO 2

--- poo/Persona.py
+++ poo/Persona.py
@@ -14,3 +14,15 @@ print(ob.getNombre())
 ob.setNombre('Ana')
 print(ob.getNombre())
 #print(type(ob))
+
+class Aprendiz(Persona):
+    def __init__(self,nombre,ficha):
+        Persona.__init__(self,nombre)
+        self.__ficha=ficha
+
+    def getFicha(self):
+        return self.__ficha
+
+app=Aprendiz('Pedro',12345)
+print(app.getFicha())
+print(app.getNombre())
