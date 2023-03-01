fbf111e815aafa3505cce6479bba3b798015dea0
padillasam <panarick@gmail.com>
2023-03-01 13:42:25 -0500

    intro POO 3

---
+++ poo/tempCodeRunnerFile.py
@@ -0,0 +1,28 @@
+class Persona:
+    nacionalidad='Colombia'    
+    def __init__(self,nombre,documento):
+        self.__nombre=nombre
+        self.__documento=documento
+    # def __init__(self):
+    #     self.nombre=''
+    #     self.documento=0
+    def datosinstancias(self):
+        print('nombre: tipo de dato',type(self.__nombre))
+        print('documento: tipo de dato',type(self.__documento))
+
+    def datos(self):
+        return self.__nombre+str(self.documento)
+    def getNombre(self):
+        return self.__nombre
+    def setNombre(self,nombre):
+        self.__nombre=nombre
+    
+
+ob1=Persona('ana',123)
+ob2=Persona('Luis',321)
+ob2.correo='luis@mail.com'
+ob2.datosinstancias()
+print(ob2.getNombre())
+ob2.setNombre('Pedro')
+print(ob2.getNombre())
+print(ob2.correo)
