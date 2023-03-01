c4922ae1afa1246dd20a2079a5d4f51c232ceaf5
padillasam <panarick@gmail.com>
2023-03-01 13:41:48 -0500

    Intro POO 3

--- poo/Persona.py
+++ poo/Persona.py
@@ -1,19 +1,36 @@
 class Persona:
-    def __init__(self,nombre):
+    nacionalidad='Colombia'    
+    def __init__(self,nombre,documento):
         self.__nombre=nombre
-        #print('Constructor Activado')        
+        self.__documento=documento
+    # def __init__(self):
+    #     self.nombre=''
+    #     self.documento=0
+    def datosinstancias(self):
+        print('nombre: tipo de dato',type(self.__nombre))
+        print('documento: tipo de dato',type(self.__documento))
 
+    def datos(self):
+        return self.__nombre+str(self.documento)
     def getNombre(self):
         return self.__nombre
-
     def setNombre(self,nombre):
         self.__nombre=nombre
+    
 
-ob=Persona('Maria')
-print(ob.getNombre())
-ob.setNombre('Ana')
-print(ob.getNombre())
-#print(type(ob))
+ob1=Persona('ana',123)
+ob2=Persona('Luis',321)
+ob2.correo='luis@mail.com'
+ob2.datosinstancias()
+print(ob2.getNombre())
+ob2.setNombre('Pedro')
+print(ob2.getNombre())
+print(ob2.correo)
+
+
+
+
+
 
 class Aprendiz(Persona):
     def __init__(self,nombre,ficha):
