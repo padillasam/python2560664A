ed14f8725ffd23f7741bdb0db75dc72d7d150f38
padillasam <panarick@gmail.com>
2023-03-04 08:48:36 -0500

    Agregqacion Composicion

---
+++ herencia/agregacion.py
@@ -0,0 +1,33 @@
+class Aprendiz:
+    def __init__(self,nombre):
+        self.__nombre=nombre
+        self.__cursos=[]
+    def agregarCurso(self,nombreCurso):
+        self.__cursos.append(nombreCurso)
+    def verCursos(self):
+        return self.__cursos
+
+class Curso:
+    def __init__(self,nombreCurso):
+        self.__nombreCurso=nombreCurso
+
+    def getNombreCurso(self):
+        return self.__nombreCurso
+    
+ob=Aprendiz('Miguel')
+c1=Curso('Python Basico')
+c2=Curso('Python Intermedio')
+c3=Curso('Java Basico')
+
+ob.agregarCurso(c1)
+ob.agregarCurso(c2)
+ob.agregarCurso(c3)
+
+for curso in ob.verCursos():
+    print(curso.getNombreCurso())
+
+del ob
+#print(ob)
+print('.....',c1.getNombreCurso())
+
+
--- herencia/composicion.py
+++ herencia/composicion.py
@@ -1,27 +1,28 @@
+class Curso:
+    def __init__(self,titulo):
+        self.__titulo=titulo
+
+    def getTitulo(self):
+        return self.__titulo
+
 class Aprendiz:
     def __init__(self,nombre):
         self.__nombre=nombre
         self.__cursos=[]
-    def agregarCurso(self,nombreCurso):
-        self.__cursos.append(nombreCurso)
-    def verCursos(self):
+
+    def agregarCurso(self,nombreCursito):
+        cursito=Curso(nombreCursito)
+        self.__cursos.append(cursito)
+
+    def getCursos(self):
         return self.__cursos
-
-class Curso:
-    def __init__(self,nombreCurso):
-        self.__nombreCurso=nombreCurso
-
-    def getNombreCurso(self):
-        return self.__nombreCurso
     
-ob=Aprendiz('Miguel')
-c1=Curso('Python Basico')
-c2=Curso('Python Intermedio')
-c3=Curso('Java Basico')
+ap=Aprendiz('Sofia')
+ap.agregarCurso('Python Basico')
+ap.agregarCurso('Python Intermedio')
 
-ob.agregarCurso(c1)
-ob.agregarCursso(c2)
-ob.agregarCurso(c3)
+for c in ap.getCursos():
+    print(c.getTitulo())
 
-for curso in ob.verCursos():
-    print(curso.getNombreCurso())
+del ap
+
